ed66c15ced646e3796f909d48b481f2c8a0dcfd1
OakLoaf <oak@beaconstudios.org>
2025-06-04 20:00:44 +0100

    Added comment

--- src/main/java/org/lushplugins/guihandler/slot/ButtonProvider.java
+++ src/main/java/org/lushplugins/guihandler/slot/ButtonProvider.java
@@ -3,6 +3,7 @@ package org.lushplugins.guihandler.slot;
 import org.jetbrains.annotations.Nullable;
 import org.lushplugins.guihandler.gui.Gui;
 
+// TODO: Consider merging with Button
 @FunctionalInterface
 public interface ButtonProvider {
     @Nullable Button button(Gui gui, Slot slot);
